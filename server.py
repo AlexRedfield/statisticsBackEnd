5591759829f6c5f99b784a6abfe8ba1ed086c6fa
Alex <877428359@qq.com>
2023-08-22 17:21:01 +0800

    add jsonify

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 import json
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
 from flask_cors import CORS, cross_origin
 from db_utils import QueryData, MyEncoder
 
 app = Flask(__name__)
-
+app.json_encoder = MyEncoder
 conn = QueryData()
 
 
@@ -14,23 +14,22 @@ conn = QueryData()
 @cross_origin()
 def get_session():
     data = conn.query_session()
+    return data
 
-    json_data = json.dumps(data, cls=MyEncoder, indent=4, ensure_ascii=False)
-    return json_data
 
 @app.route("/playtime")
 @cross_origin()
 def get_playtime():
-    data=conn.query_game_time()
-    json_data = json.dumps(data, cls=MyEncoder, indent=4, ensure_ascii=False)
-    return json_data
+    data = conn.query_game_time()
+    return jsonify(data)
+
 
 @app.route("/re_playtime")
 @cross_origin()
 def get_re_playtime():
-    data=conn.query_game_time_format()
-    json_data = json.dumps(data, cls=MyEncoder, indent=4, ensure_ascii=False)
-    return json_data
+    data = conn.query_game_time_format()
+    return jsonify(data)
+
 
 if __name__ == "__main__":
     app.debug = True
